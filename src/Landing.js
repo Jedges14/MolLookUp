f5b5117e2d593dd71ef00103207bb4342f09581f
Jedges14 <jedgesgyasi14@gmail.com>
2025-06-23 21:22:35 -0400

    latest update for css styling and background update

--- src/Landing.js
+++ src/Landing.js
@@ -1,8 +1,9 @@
 import './Landing.css';
 
 const LandingPage = () => {
+
+    
     return (
-        
         <>
         <div className ="Title_Box">
             {/* three bar logo for menu lives here */}
@@ -15,29 +16,37 @@ const LandingPage = () => {
                     </ul>
                 </nav>
             </div>
+
             <div className="wesbiteName">
                 <h2>MolLookUp</h2>
             </div>
+
             {/* user icon lives here */}
-            <div className="UserLogin"><h3>UserLogin</h3></div>
+            <div className="UserLogin">
+                <h3>UserLogin</h3>
+            </div>
         </div>
 
         <div className="fact-Container">
-            <div className="Random-Mol-fact"></div>
-            <div className="Mol-History"></div>
+            <div className="Random-Mol-fact"><p>free</p></div>
+            <div className="Mol-History"><p>part</p></div>
         </div>
 
         {/* input the search */}
         <div className="Search-Container">
             <form>
                 <label for ="input">
-                    <p className="style input search header">Find chemical compounds, see 3D models, and explore molecular data.</p>
+                    <p className="inputText" >
+                        Find chemical compounds, see 3D models, and explore molecular data.
+                    </p>
                 </label>
-                <input type="text" id="input" name="search" placeholder='Enter molecule name here'></input>
-            </form>
+                <center>
+                    <input type="text" id="input" name="search" placeholder='Enter molecule name here'></input>
+                </center> 
+           </form>
         </div>
 
-        <div className="description-field area"></div>
+        <div className="description-field area" ></div>
 
         <div className="footer-Area">
             <footer></footer>
